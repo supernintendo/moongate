fabf8520b4db285a43e4c8410d930b899776ad48
Michael Matyi <michaelmatyi@icloud.com>
2015-02-16 23:32:50 -0800

    master > Entities in table.

--- client/game/network/entities.lua
+++ client/game/network/entities.lua
@@ -12,7 +12,34 @@ function EntityState:receive(packet)
 end
 
 function EntityState:updateEntities(value)
-  print(inspect(value))
+  partials = {}
+  entities = {}
+
+  for partial in string.gmatch(value, '([^|]+)') do
+    table.insert(partials, partial)
+  end
+
+  for i, partial in ipairs(partials) do
+    local id = nil
+    local x = nil
+    local y = nil
+
+    for attribute in string.gmatch(partial, '([^;]+)') do
+      if not id then id = attribute
+      elseif not x then x = attribute
+      elseif not y then y = attribute
+      end
+      if id and x and y then
+        table.insert(entities, {
+          id = id,
+          x = x,
+          y = y
+        })
+      end
+    end
+  end
+
+  self.contents = entities
   self.update = true
 end
 
