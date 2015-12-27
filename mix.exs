da4bf9f80e3033c98dd7f04bdf32fe49618e9968
Michael Matyi <michaelmatyi@icloud.com>
2015-12-27 12:40:09 -0800

    master > another attempt at docs

--- mix.exs
+++ mix.exs
@@ -18,7 +18,9 @@ defmodule Moongate.Mixfile do
   defp deps do
     [
       {:cowboy, "1.0.3"},
+      {:earmark, "~> 0.1", only: :dev},
       {:ecto, "~> 1.0.2"},
+      {:ex_doc, "~> 0.11", only: :dev},
       {:inch_ex, only: :docs},
       {:json, "~> 0.3.2"},
       {:pbkdf2, ">= 2.0.0", github: "basho/erlang-pbkdf2"},
