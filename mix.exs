e4c2ef05e1dd21e3c7a2e4544046d1f7b34a350c
Michael Matyi <michaelmatyi@icloud.com>
2015-12-27 12:47:31 -0800

    master > removing inch-ci

--- mix.exs
+++ mix.exs
@@ -21,7 +21,6 @@ defmodule Moongate.Mixfile do
       {:earmark, "~> 0.1", only: :dev},
       {:ecto, "~> 1.0.2"},
       {:ex_doc, "~> 0.11", only: :dev},
-      {:inch_ex, only: :docs},
       {:json, "~> 0.3.2"},
       {:pbkdf2, ">= 2.0.0", github: "basho/erlang-pbkdf2"},
       {:postgrex, ">= 0.7.0"},
