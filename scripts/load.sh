44193511333af0cfca98fba44df8d8c2dce3a60a
Michael Matyi <michaelmatyi@icloud.com>
2017-06-21 12:41:09 -0700

    consolidate bash scripts

--- scripts/load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-elixir scripts/moongate_config.exs
-echo -e "Loading \033[0;32m${GAME_PATH}\033[0m"
-mix clean
-elixir scripts/gen_rustler_bridge.exs
-iex --dot-iex .iex.moongate.exs -S mix
--- scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-MIX_ENV=test mix clean
-MIX_ENV=test mix test
